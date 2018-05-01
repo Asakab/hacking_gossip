e41b15391ac35a6cf1d80420e0c3448de88aad1a
Selma <asakab@live.fr>
2018-05-01 14:26:01 +0200

    third one

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -4,10 +4,21 @@ class GossipsController < ApplicationController
 
   def new
   	@gossip = Gossip.new
+  	
+  	
   end
 
   def create
-    @gossip = Gossip.new
+    @gossip = Gossip.new(anonymous_author: params["anonymous_author"], content: params["content"])
+   if @gossip.save 
+     redidrect_to gossips_path
+
+   # @gossip.anonymous_author = params[:name]
+    #@gossip.content = params[:content]
+    #@gossip.save
+    #redirect_to ?
+
+end
   end 
  
 
