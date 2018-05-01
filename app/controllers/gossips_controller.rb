63974d1ba59498030b78e559eb22c0825091fb4f
Selma <asakab@live.fr>
2018-05-01 15:46:33 +0200

    please me lâche pas

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -9,16 +9,15 @@ class GossipsController < ApplicationController
   end
 
   def create
-    @gossip = Gossip.new(anonymous_author: params["anonymous_author"], content: params["content"])
-   if @gossip.save 
-     redidrect_to gossips_path
+  	@gossip = Gossip.new(anonymous_author: params["anonymous_author"], content: params["content"])
+    @gossip.save 
+    
 
    # @gossip.anonymous_author = params[:name]
     #@gossip.content = params[:content]
     #@gossip.save
     #redirect_to ?
 
-end
   end 
  
 
