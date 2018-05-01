52a634771685c149b100fe675ac5ea6c20f721dc
Selma <asakab@live.fr>
2018-05-01 18:26:12 +0200

    le new form

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -10,15 +10,12 @@ class GossipsController < ApplicationController
 
   def create
   	@gossip = Gossip.new(anonymous_author: params[:anonymous_author], content: params[:content])
-       @gossip.save 
-       # redirect_to gossips_path
-     end
+     
+      @gossip.save 
+       # redirect_to post_gossips_path(@gossip.id)
+     
+ end
     
-
-   # @gossip.anonymous_author = params[:name]
-    #@gossip.content = params[:content]
-    #@gossip.save
-    #redirect_to ?
 
 
 
