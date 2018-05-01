4179844dcd6978c23e79eebab9c3c185d5a87fb3
Selma <asakab@live.fr>
2018-05-01 16:45:16 +0200

    toujours en PLS

--- app/controllers/gossips_controller.rb
+++ app/controllers/gossips_controller.rb
@@ -9,9 +9,10 @@ class GossipsController < ApplicationController
   end
 
   def create
-  	@gossip = Gossip.new(anonymous_author: params["anonymous_author"], content: params["content"])
- 
-    @gossip.save 
+  	@gossip = Gossip.create(anonymous_author: params[:anonymous_author], content: params[:content])
+       @gossip.save 
+        #redirect_to gossips_path(@gossip.id)
+     end
     
 
    # @gossip.anonymous_author = params[:name]
@@ -19,8 +20,7 @@ class GossipsController < ApplicationController
     #@gossip.save
     #redirect_to ?
 
-  end 
- 
+
 
   def show
   end
